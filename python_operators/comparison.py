7d7411eb3cb919e8e61a844c3e6e4a67a2fc0c26
Efidar <94045398+Efidar63@users.noreply.github.com>
2023-09-10 11:37:00 +0200

    conditionals

---
+++ python_operators/comparison.py
@@ -0,0 +1,24 @@
+#username, password=>database
+#'efidarkeskin' ,'123456'
+
+a,b,c,d=5,5,10,4
+password='1234'
+username='efidarkeskin'
+
+result=(a==b) #True
+result=(a==c) #False
+
+result = ('fdrkskn'== username)
+result = ('efidarkeskin'== username)
+result = (a != b)
+result = (a != c)
+result = (a > c)
+result = (a < c)
+result = (a >= b)
+result = (c <= b)
+result = (True == 1)
+result = (False == 0)
+result = False + True + 40
+
+print(result)
+
