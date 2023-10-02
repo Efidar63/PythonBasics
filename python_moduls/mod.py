ddd7e1d687ad8bbcb2732a6652cc41df6c4124c9
Efidar <94045398+Efidar63@users.noreply.github.com>
2023-10-02 16:34:32 +0300

    moduls

---
+++ python_moduls/mod.py
@@ -0,0 +1,22 @@
+'''
+information about module
+'''
+print('module added')
+number=10
+numbers=[1,2,3]
+person={
+    "name":"efidar",
+    "age":"22",
+    "city":"germany"
+
+}
+
+def func(x):
+    '''
+    information about our function
+    '''
+    print(f'x {x}')
+
+    class Person:
+        def speak(self):
+            print('I am speaking')
