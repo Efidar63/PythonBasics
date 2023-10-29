0a4ae288781a87a47deda7f650ed8eee1fc1052b
Efidar <94045398+Efidar63@users.noreply.github.com>
2023-10-29 12:18:06 +0300

    requests

---
+++ python_advanced_moduls/_request.py
@@ -0,0 +1,18 @@
+# import json
+# print(json.__file__)
+
+
+import requests
+import json
+
+result = requests.get("https://jsonplaceholder.typicode.com/todos")
+result = json.loads(result.text)
+
+# print(result[0]["title"])
+# print(result[0])
+# for i in result:
+#     print(i)
+for i in result:
+    if i["userId"]==1:
+        print(i["title"])
+print(type(result))
